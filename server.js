2d083f0386abec68a8b92394b6ea104be2f3850d
Himanshu Gauba <hgauba4@gmail.com>
2025-04-28 01:43:41 +0530

    change port through env

--- server.js
+++ server.js
@@ -251,5 +251,5 @@ app.get('/me', authMiddleware, async (req, res) => {
     res.json({ username: user.username, credits: user.credits });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
